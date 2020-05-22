6ff447a0511535b4d23fa01594aaa10f7d1b71ec
jedimasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2020-05-22 20:07:13 +0100

    Initial working version

---
+++ index.js
@@ -0,0 +1,38 @@
+const {google} = require("googleapis");
+const {auth} = require("google-auth-library");
+const sql = google.sql('v1beta4');
+
+const project = process.env.PROJECT;
+const instance = process.env.INSTANCE;
+const bucket = process.env.BUCKET;
+const db = process.env.DB;
+
+exports.exportDatabase = (_req, res) => {
+  async function doIt() {
+    const authRes = await auth.getApplicationDefault();
+    let authClient = authRes.credential;
+    var request = {
+      project: `${project}`,
+      instance: `${instance}`,
+      resource: {
+        exportContext: {
+          kind: "sql#exportContext",
+          fileType: "SQL",
+          uri: `gs://${bucket}/backup-${Date.now()}.gz`,
+          databases: [`${db}`]
+        }
+      },
+      auth: authClient
+    };
+
+    sql.instances.export(request, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
+      res.status(200).send("Command completed", err, result); 
+    });
+  }
+  doIt();
+};
